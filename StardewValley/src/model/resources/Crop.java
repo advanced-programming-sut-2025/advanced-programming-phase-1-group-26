6d974ab2cb9026b66a813a4a0e2826b6cae9248a
Mohsen <mohsensalah128@gmail.com>
2025-05-03 21:29:29 +0330

    completed crops and trees

--- StardewValley/src/model/resources/Crop.java
+++ StardewValley/src/model/resources/Crop.java
@@ -1,30 +1,18 @@
 package model.resources;
 
-import model.GameObject;
 import model.enums.resources_enums.CropType;
-import model.enums.resources_enums.ForagingSeedType;
-import model.enums.Season;
 
-import java.util.List;
-
-public class Crop extends GameObject
+public class Crop extends CropsAndTrees
 {
-    private CropType cropType;
-    private ForagingSeedType seedType;
-    private List<Integer> stages;
-    private int totalHarvestTime;
     private boolean oneTime;
     private int growthTime;
-    private int baseSellPrice;
-    private boolean isEdible;
-    private int energy;
-    private List<Season> seasons;
     private boolean canBecomeGiant;
 
     public Crop(CropType cropType)
     {
-        this.cropType = cropType;
-        this.seedType = cropType.getSeedType();
+        this.type = cropType;
+        this.name = cropType.getName();
+        this.source = cropType.getSeedType();
         this.stages = cropType.getStages();
         this.totalHarvestTime = cropType.getTotalHarvestTime();
         this.oneTime = cropType.isOneTime();
@@ -34,6 +22,11 @@ public class Crop extends GameObject
         this.energy = cropType.getEnergy();
         this.seasons = cropType.getSeasons();
         this.canBecomeGiant = cropType.isCanBecomeGiant();
-        this.ObjectType = cropType.getT
+//        this.ObjectType = cropType.getT
+    }
+
+    public String getName()
+    {
+        return this.name;
     }
 }
--- StardewValley/src/model/resources/Tree.java
+++ StardewValley/src/model/resources/Tree.java
@@ -2,32 +2,27 @@ package model.resources;
 
 import model.GameObject;
 import model.enums.Season;
+import model.enums.resources_enums.FruitType;
 import model.enums.resources_enums.TreeType;
 
 import java.util.List;
 
-public class Tree extends GameObject
+public class Tree extends CropsAndTrees
 {
-    private final TreeType treeType;
-    private String source;
-    private List<Integer> stages;
-    private int totalHarvestTime;
-    private String fruitName;
+    private FruitType fruit;
     private int fruitHarvestCycle;
-    private int fruitBaseSellPrice;
-    private boolean isEdible;
-    private int energy;
     private List<Season> seasons;
 
     public Tree(TreeType treeType)
     {
-        this.treeType = treeType;
+        this.type = treeType;
+        this.name = treeType.getName();
         this.source = treeType.getSource();
         this.stages = treeType.getStages();
         this.totalHarvestTime = treeType.getTotalHarvestTime();
-        this.fruitName = treeType.getFruitName();
+        this.fruit = treeType.getFruit();
         this.fruitHarvestCycle = treeType.getFruitHarvestCycle();
-        this.fruitBaseSellPrice = treeType.getFruitBaseSellPrice();
+        this.baseSellPrice = treeType.getFruitBaseSellPrice();
         this.isEdible = treeType.isEdible();
         this.energy = treeType.getEnergy();
         this.seasons = treeType.getSeasons();
