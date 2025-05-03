3cff95ee6adeb4bd4d443fcd7f59315bff7a7893
Moshtagh03 <moshtagh.motazedian@gmail.com>
2025-05-03 22:05:15 +0330

    doing some shit for animals

--- StardewValley/src/model/animal/AnimalBuilding.java
+++ StardewValley/src/model/animal/AnimalBuilding.java
@@ -1,9 +1,27 @@
 package model.animal;
 
-import model.enums.animal_enums.AnimalBuildingType;
+import model.enums.animal_enums.FarmBuilding;
 
 public class AnimalBuilding {
-    private AnimalBuildingType animalBuildingType;
+    private FarmBuilding farmBuilding;
     private int capacity;
     private int cost;
+
+    public AnimalBuilding(FarmBuilding farmBuilding) {
+        this.farmBuilding = farmBuilding;
+        this.capacity = farmBuilding.getCapacity();
+        this.cost = farmBuilding.getPrice();
+    }
+
+    public FarmBuilding getFarmBuilding() {
+        return farmBuilding;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getCost() {
+        return cost;
+    }
 }
---
+++ StardewValley/src/model/enums/animal_enums/SizeOfAnimalBuildings.java
@@ -0,0 +1,7 @@
+package model.enums.animal_enums;
+
+public enum SizeOfAnimalBuildings {
+    NORMAL,
+    BIG,
+    DELUXE
+}
