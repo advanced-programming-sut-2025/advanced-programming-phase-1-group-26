f96fb64145003455c3bb0c92fc6aac7589ebde8e
brokearash <arashakbari2006@gmail.com>
2025-05-15 12:11:28 +0330

    skill level updated

--- StardewValley/src/model/player_data/Skill.java
+++ StardewValley/src/model/player_data/Skill.java
@@ -1,4 +1,5 @@
 package model.player_data;
+import model.Player;
 import model.enums.SkillType;
 
 public class Skill {
@@ -58,8 +59,64 @@ public class Skill {
         return unit;
     }
 
-    public void addUnit(int unit) {
-        this.unit += unit;
+    public void changeUnit(int delta) {
+        this.unit += delta;
+
+        if (delta >= 0) handleLevelUp();
+        else handleLevelDown();
+
+    }
+
+    private void handleLevelUp() {
+        while (true) {
+            switch (this.level) {
+                case 0:
+                    if (unit >= 50) levelUp(50);
+                    else return;
+                    break;
+                case 1:
+                    if (unit >= 150) levelUp(150);
+                    else return;
+                    break;
+                case 2:
+                    if (unit >= 250) levelUp(250);
+                    else return;
+                    break;
+                case 3:
+                    if (unit >= 350) levelUp(350);
+                    else return;
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+
+    private void handleLevelDown() {
+        while (this.level > 0) {
+            int threshold = getThresholdForLevel(this.level - 1);
+            if (this.unit < 0) {
+                this.level--;
+                this.unit += threshold;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void levelUp(int threshold) {
+        this.level++;
+        this.unit -= threshold;
+    }
+
+    private int getThresholdForLevel(int level) {
+        return switch (level) {
+            case 0 -> 50;
+            case 1 -> 150;
+            case 2 -> 250;
+            case 3 -> 350;
+            default -> Integer.MAX_VALUE;
+        };
     }
 
     public void setUnit(int unit) {
